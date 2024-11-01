e73cc4b430ab2378fee320f9de9b74c325661e83
xcoderdevesh <xcoder45@gmail.com>
2024-11-01 18:01:30 +0530

    Updates

---
+++ precache-manifest.1b3507405c516db21015cca882a3f4a8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996fd5ce91876e58ff2c3d73fcfde57d",
+    "url": "/portfolio-devesh-kumar/index.html"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/portfolio-devesh-kumar/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "a31d2682b532782545ef",
+    "url": "/portfolio-devesh-kumar/static/js/main.c516c685.chunk.js"
+  },
+  {
+    "revision": "44fa3005a84e0c4ba234",
+    "url": "/portfolio-devesh-kumar/static/js/runtime~main.43ef61bc.js"
+  }
+]);
